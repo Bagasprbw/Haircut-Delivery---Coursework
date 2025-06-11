e0f61d371ebd7646cdf1c67e0d05e218b0e7ed67
Bagas Prabowo <bagasprabowo2412@gmail.com>
2025-06-11 19:26:35 +0700

    ajax

--- register.php
+++ register.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <style>
         body {
@@ -64,6 +65,7 @@
                         <div class="col">
                             <label for="username" class="form-label">Username</label>
                             <input type="username" class="form-control shadow" id="username" name="username" placeholder="Enter your username">
+                            <div id="usernameFeedback" class="form-text text-danger"></div>
                         </div>
                     </div>
                     <div class="mb-3">
@@ -92,5 +94,26 @@
 
     <!-- Bootstrap 5.3.3 JS -->
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="jquery.js"></script>
+    <script>
+    $(document).ready(function () {
+        $('#username').on('keyup', function () {
+            var username = $(this).val();
+            if (username.length > 1) {
+                $.ajax({
+                    url: 'Controller/check_username.php',
+                    method: 'POST',
+                    data: { username: username },
+                    success: function (response) {
+                        $('#usernameFeedback').html(response);
+                    }
+                });
+            } else {
+                $('#usernameFeedback').html('');
+            }
+        });
+    });
+    </script>
+
 </body>
 </html>
